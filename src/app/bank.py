7e520e2380253b4dc042a281b0e70c86511a96b1
Marc <marcsauer@web.de>
2019-02-05 16:58:27 +0100

    Implementierung

---
+++ src/app/bank.py
@@ -0,0 +1,31 @@
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = []
+        self.transactions = []
+
+    def open_account(self, account):
+        assert account not in self.accounts, 'Account number 1 already taken!'
+        self.accounts.append(account)
+        return account
+
+    def add_transaction(self, sender, recipient, subject, amount):
+        _transaction = {
+            'sender':sender,
+            'recipient': recipient,
+            'subject': subject,
+            'amount': amount
+        }
+        assert sender in self.transactions, 'Sender has no account yet!'
+        assert recipient in self.transactions, 'Recipient has no account yet!'
+        assert amount > 0, 'Amount has to be greater than 0'
+        self.transactions.append(_transaction)
+        return _transaction
+
+
+
+
+
+
+
+
