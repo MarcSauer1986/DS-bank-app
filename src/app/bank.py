41d01cfcb89d239478e2af0090628a27cbe3be0d
Marc <marcsauer@web.de>
2019-02-06 10:21:43 +0100

    Bank implement

--- src/app/bank.py
+++ src/app/bank.py
@@ -1,29 +1,24 @@
 class Bank:
+
     def __init__(self, name):
         self.name = name
         self.accounts = []
         self.transactions = []
+        pass
 
     def open_account(self, account):
-        assert account not in self.accounts, 'Account number 1 already taken!'
         self.accounts.append(account)
         return account
 
     def add_transaction(self, sender, recipient, subject, amount):
-        _transaction = {
+        assert amount > 0, 'Amount has to be greater than 0'
+        transaction = {
             'sender':sender,
             'recipient': recipient,
-            'subject': subject,
-            'amount': amount
+            'subject':subject,
+            'amount':amount
         }
-        assert amount > 0, 'Amount has to be greater than 0'
-        self.transactions.append(_transaction)
-        return _transaction
-
-
-
-
-
-
+        self.transactions.append(transaction)
+        return transaction
 
 
