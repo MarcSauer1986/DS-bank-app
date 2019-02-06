5beaff1bfeaaadaaa1298d702acdd66f0331b769
Marc <marcsauer@web.de>
2019-02-06 10:34:25 +0100

    Implement bank

--- src/app/bank.py
+++ src/app/bank.py
@@ -7,11 +7,17 @@ class Bank:
         pass
 
     def open_account(self, account):
+        account_number = account['number']
+        assert not self._account_exists(account), f'Account number {account_number} already taken!'
+
         self.accounts.append(account)
         return account
 
-    def add_transaction(self, sender, recipient, subject, amount):
+    def add_transaction(self, *, sender, recipient, subject, amount):
         assert amount > 0, 'Amount has to be greater than 0'
+        assert self._account_exists(sender), 'Sender has no account yet!'
+        assert self._account_exists(recipient), 'Recipient has no account yet!'
+
         transaction = {
             'sender':sender,
             'recipient': recipient,
@@ -20,5 +26,12 @@ class Bank:
         }
         self.transactions.append(transaction)
         return transaction
+
+    def _account_exists(self, account_to_search):
+        matches = list(filter(
+            lambda account: account == account_to_search,
+            self.accounts))
+
+        return matches and len(matches) == 1
 
 
