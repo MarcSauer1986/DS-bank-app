6e1b7c697c8c66c2a412066a74751304134af38a
Marc <marcsauer@web.de>
2019-02-07 14:48:59 +0100

    Lesson 2 complete

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,4 +2,4 @@ from .bank import Bank
 from .account import Account
 from .transaction import Transaction
 
-__all__ = ['Bank', 'Account']
+__all__ = ['Bank', 'Account', 'Transaction']
--- src/app/bank.py
+++ src/app/bank.py
@@ -1,3 +1,22 @@
+import app
+
+
 class Bank:
     def __init__(self, name):
         self.name = name
+        self.accounts = {}
+        self.transactions = []
+
+    def open_account(self, account):
+        assert type(account) == app.Account, 'Account should be an app.Account'
+        assert account.number not in self.accounts, f'Account number {account.number} already taken!'
+        self.accounts[account.number] = account
+        return account
+
+    def add_transaction(self, *, sender, recipient, subject, amount):
+        assert amount > 0, 'Amount needs to be greater than 0'
+        assert sender.number in self.accounts, 'Sender has no account yet!'
+        assert recipient.number in self.accounts, 'Recipient has no account yet!'
+        self.transactions.append(app.Transaction(sender=sender.number, recipient=recipient.number, subject=subject,
+                                                 amount=amount))
+        return self.transactions[-1]
