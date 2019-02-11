03219e92c9a5f8fa0278cce7a88343055790e2d5
Marc <marcsauer@web.de>
2019-02-11 13:12:05 +0100

    Implement bank with pandas

--- src/app/bank.py
+++ src/app/bank.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import app
 
+
 class Bank:
+
     def __init__(self, name):
         self.name = name
         self.accounts = pd.DataFrame(columns=['id', 'firstname', 'lastname', 'balance'])
         self.transactions = pd.DataFrame(columns=['id', 'sender_id', 'recipient_id', 'amount', 'subject', 'category',
                                                   'timestamp'])
+
     def open_account(self, *, account_id, firstname, lastname, balance=0.0):
-        assert not self._has_valid_account(account_id), f'Account number {account_id} already taken!'
         account = {'id': account_id, 'firstname': firstname, 'lastname': lastname, 'balance': balance}
+        assert not self._has_valid_account(account_id), f'Account number {account_id} already taken!'
         self.accounts = self.accounts.append(account, ignore_index=True)
         return self.accounts
 
-    def add_transaction(self, *, transaction_id,
-                        sender_id,
-                        recipient_id,
-                        subject,
-                        amount,
-                        category,
-                        timestamp):
+    def add_transaction(self, *, transaction_id, sender_id, recipient_id, subject, amount, category, timestamp):
         transaction = {'id': transaction_id, 'sender_id': sender_id, 'recipient_id': recipient_id, 'amount': amount,
                        'subject': subject, 'category': category, 'timestamp': timestamp}
         assert app.check_greater_zero(amount), 'Amount needs to be greater than 0'
