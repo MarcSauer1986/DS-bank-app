7a5697f9569a2905d1deae6f9dc287801e561388
Marc <marcsauer@web.de>
2019-02-11 12:51:27 +0100

    Lession 3

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,3 +1,9 @@
 from .bank import Bank
 
+def check_greater_zero(value):
+	    if isinstance(value, int):
+	        return value > 0
+	    elif isinstance(value, float):
+	        return value > 0.0
+
 __all__ = ['Bank']
